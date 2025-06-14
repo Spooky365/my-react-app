f6d0392f7907a6a2374bd5d26588f3cbdb155e71
Ich <you@example.com>
2025-06-14 10:34:49 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg'; // create-react-app includes this by default
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from my React App!</h1>
+        <p>
+          This application is running inside a Docker container, served by NGINX.
+        </p>
+        <p>
+          Next stop: Kubernetes via FluxCD!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
